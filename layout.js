18300bcdde44cfeef49dd34c2fa3d9892624a667
CoutinhoSUD <144079383+Coutinho33@users.noreply.github.com>
2026-04-21 21:10:04 -0300

    Add files via upload
    export const metadata = {
      title: "Núcleo de Estudos da Fé"
    };
    
    export default function RootLayout({ children }) {
      return (
        <html lang="pt-br">
          <body>{children}</body>
        </html>
      );
    }

---
+++ layout.js
@@ -0,0 +1,6 @@
+
+export const metadata = { title: 'Núcleo de Estudos da Fé' };
+import './globals.css';
+export default function RootLayout({children}) {
+ return <html lang="pt-br"><body>{children}</body></html>
+}
