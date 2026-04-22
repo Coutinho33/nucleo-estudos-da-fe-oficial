53210224b4d8955d930f7e62df29aa970d3c8ef2
CoutinhoSUD <144079383+Coutinho33@users.noreply.github.com>
2026-04-21 21:28:45 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,11 @@
+export const metadata = {
+  title: "Núcleo de Estudos da Fé"
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
